fff2c183e3885ec6e200526296dd2b4f1bb71b0c
sudrajadhadi <37056267+sudrajadhadi@users.noreply.github.com>
2019-04-05 14:00:02 +0700

    Create server-pembeli.c

---
+++ soal2/server-pembeli.c
@@ -0,0 +1,116 @@
+  #include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <pthread.h>
+#define PORT 8080
+
+void *kurang(void *k) {
+	int *barang;
+	barang=(void *)k;
+	if(*barang>0) {
+		*barang-=1;
+	}
+}
+
+void *cetak(void *c) {
+	int *barang;
+	barang=(void *)c;
+	printf("STock barang = %d\n", *barang); 
+	sleep(5);
+}
+
+int main(int argc, char const *argv[]) {
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+      
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+      
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons( PORT );
+      
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+
+	key_t key = 1234;
+        int *barang;
+
+        int shmid = shmget(key, sizeof(int), IPC_CREAT | 0666);
+        barang = shmat(shmid, NULL, 0);
+
+	char *gagal="Transaksi Gagal";
+	char *berhasil="Transaksi Berhasil";
+	char *beli="Beli";
+
+        //shmdt(value);
+        //shmctl(shmid, IPC_RMID, NULL);
+
+    valread = read( new_socket , buffer, 1024);
+    //printf("%s\n",buffer );
+    if(*barang<=0) {
+    	send(new_socket , gagal , strlen(gagal) , 0 );
+    }
+
+    else  {
+    	send(new_socket , berhasil , strlen(berhasil) , 0 );
+   } 
+   //printf("Hello message sent\n");
+
+//-----------------------Thread-----------------------
+
+    pthread_t thread1, thread2;//inisialisasi awal
+    const char *message1 = "Thread 1";
+    int  iret1, iret2;
+
+	  if(strcmp(buffer, beli)==0) {
+	    	iret1 = pthread_create( &thread1, NULL, kurang, (void*) barang); //membuat thread pertama
+	    	if(iret1) //jika eror
+	    	{
+			fprintf(stderr,"Error - pthread_create() return code: %d\n",iret1);
+			exit(EXIT_FAILURE);
+	    	}
+	    	pthread_join( thread1, NULL);
+	    }
+
+	while(1) {
+	    iret2 = pthread_create( &thread2, NULL, cetak, (void*) barang);//membuat thread kedua
+	    if(iret2)//jika gagal
+	    {
+		fprintf(stderr,"Error - pthread_create() return code: %d\n",iret2);
+		exit(EXIT_FAILURE);
+	    }
+	    pthread_join( thread2, NULL); 
+	}
+
+exit(EXIT_SUCCESS);
+   return 0;
+}
